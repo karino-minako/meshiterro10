40b17883b968deaaa257bdf6ac27aeea328c0c3e
vagrant <vagrant@localhost.localdomain>
2020-05-12 08:28:51 +0000

    [Review Finish] 5-4

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,9 +1,10 @@
 class PostImagesController < ApplicationController
+
 	def new
 		@post_image = PostImage.new
-	end
+    end
 
-	def create
+    def create
         @post_image = PostImage.new(post_image_params)
         @post_image.user_id = current_user.id
         if @post_image.save
@@ -13,15 +14,22 @@ class PostImagesController < ApplicationController
         end
     end
 
-	def index
-		@post_images = PostImage.all
+    def index
+  		@post_images = PostImage.page(params[:page]).reverse_order
 	end
 
-	def show
-		@post_image = PostImage.find(params[:id])
+    def show
+	    @post_image = PostImage.find(params[:id])
+	    @post_comment = PostComment.new
 	end
 
-private
+	def destroy
+	    @post_image = PostImage.find(params[:id])
+	    @post_image.destroy
+	    redirect_to post_images_path
+	end
+
+    private
 
     def post_image_params
         params.require(:post_image).permit(:shop_name, :image, :caption)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_051421) do
+ActiveRecord::Schema.define(version: 2020_05_12_070845) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_comments", force: :cascade do |t|
     t.text "comment"
